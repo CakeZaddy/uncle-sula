a27a248745c32028d60b98a54a4d029ee37cf145
Susudef <Susudef2u@gmail.com>
2023-03-31 22:27:20 +0100

    Dark toggle test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from './Components/Footer'
 import Header from './Components/Header'
 import Home from './pages/Home'
+import { useState } from 'react'
+import { MdDarkMode, MdLightMode } from 'react-icons/md'
 
 function App() {
+  const [darkToggle, setDarkToggle] = useState(false)
+
   return (
-    <body class='dark:bg-slate-800 dark:text-white '>
+    <body
+      class={`relative dark:bg-slate-800 dark:text-white ${
+        darkToggle && 'dark'
+      }`}
+    >
+      <div class='float'>
+        <MdDarkMode
+          onClick={() => setDarkToggle(true)}
+          class='absolute top-[3%] left-5 z-50 text-3xl text-black '
+        />
+        <MdLightMode
+          onClick={() => setDarkToggle(false)}
+          class='absolute top-[3%] right-5 z-50 text-3xl '
+        />
+      </div>
       <Router>
         <Header />
         <Routes>
